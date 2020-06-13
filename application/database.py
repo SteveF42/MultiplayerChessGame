53cee55bc0e529bdd5c47c74d9b58ac5aec5044e
repl.it user <replituser@example.com>
2020-06-13 21:57:08 +0000

    added database

---
+++ application/database.py
@@ -0,0 +1,106 @@
+import sqlite3
+
+TABLE = 'users'
+
+def create_database_user(name,email,password):
+  Dictionary  = {'name':name, 'email':email,'password':password}
+  return Dictionary 
+  #return new dictionary that stores the user information
+
+class Database:
+  def __init__(self):
+    self.conn = sqlite3.connect('userInfo.db')
+    self.conn.row_factory = self._row_facatory
+    self.cursor = self.conn.cursor()
+    self.table=self._create_table()
+
+  def _row_facatory(self,cursor, row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+  def _create_table(self):
+    query = f"""CREATE TABLE IF NOT EXISTS {TABLE}(
+      name text,
+      email text,
+      password text,
+      wins intiger,
+      losses intiger,
+      id INTEGER PRIMARY KEY AUTOINCREMENT 
+    )"""
+    with self.conn:
+      self.cursor.execute(query)
+
+  def insert_new_user(self, user):
+    '''
+    type user: disctionary
+    rtype: None
+    '''
+    data = (user['name'],user['email'],user['password'],0,0,None)
+    with self.conn:
+      query = f'''INSERT INTO {TABLE} VALUES(?,?,?,?,?,?)'''
+      self.cursor.execute(query,data)
+  
+  def update_wins(self, user):
+    """
+    type user: dictionary
+    rtype: None
+    """
+    # values_to_update = []
+    # for key in values.keys():
+    #   values_to_update.append(f'{key} = {values[key]} ')
+      
+    # string_from_vals = ' '.join(values_to_update)
+    # print('[UPDATING DB]',string_from_vals)
+    
+
+    with self.conn:
+      query = f"UPDATE {TABLE} SET wins = wins + 1 WHERE name =:name AND email =:email "
+      self.cursor.execute(query,{'name':user['name'],'email':user['email']})
+  def update_loss(self, user):
+    '''
+    type user: Dict 
+    rtype: None
+    '''
+    with self.conn:
+      query ='UPDATE {TABLE} SET losses = losses + 1 WHERE name =:name'
+      self.cursor.execute(query,{'name':user['name']})
+
+  def validate_user(self, user):
+    '''
+    type user: Dict
+    rtype bool
+    '''
+    with self.conn:
+      query = f'SELECT * FROM {TABLE} where name=:name AND email =:email AND password =:password'
+      self.cursor.execute(query,{'name':user['name'], 'email':user['email'], 'password':user['password']})
+
+    value = self.cursor.fetchall()
+    return True if value else False
+
+  def get_user_info(self, userInfo):
+    '''
+    type userInfo: Dict
+    rtype: Dict
+    '''
+    with self.conn:
+      query = f'SELECT * FROM {TABLE} WHERE name =:name OR email =:email'
+      self.cursor.execute(query,{'name':userInfo['name'],'email':userInfo['email']})
+    data = self.cursor.fetchone()
+    return data
+
+
+
+if __name__ == '__main__':
+  c = Database()
+  user = {
+    'name':'josh',
+    'email':'josh@gmail.com',
+    'password' : 'uhfera88'
+  }
+  print(c.get_user_info(user))
+  print(c.validate_user(user))
+  # vals = ['wins','losses']
+  # c.update_data(vals)
+      
